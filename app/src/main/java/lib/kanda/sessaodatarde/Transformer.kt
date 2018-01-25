7c7569ceff30b997598a974230548b027128c9dd
Caique Oliveira <j.caique_fla@hotmail.com>
2018-01-25 17:56:02 -0200

    update transformer

--- app/src/main/java/lib/kanda/sessaodatarde/Transformer.kt
+++ app/src/main/java/lib/kanda/sessaodatarde/Transformer.kt
@@ -1,9 +1,11 @@
 package lib.kanda.sessaodatarde
 
-import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 
@@ -11,22 +13,38 @@ typealias Retry = Int
 
 /**
  * Created by jcosilva on 1/24/2018.
- * Este transformer deve receber um estado de Pooling e executar um número de tentativas que podem ser definidas no tra
+ * Este transformer deve receber um estado de Polling e executar um número de tentativas que podem ser definidas no tra
  * Para o desenvolvimento da solução foi pesquisado sobre backOff Rx
  **/
-class Transformer1(val retries: Retry = 4) : ObservableTransformer<Any, Any> {
+class Transformer1(val retries: Retry = 4, val scheduler: Scheduler = Schedulers.trampoline()) : ObservableTransformer<Any, Any> {
     override fun apply(upstream: Observable<Any>): ObservableSource<Any> {
         return upstream.retryWhen { errors ->
-            errors.scan(0, { errorCount, err ->
-                if (errorCount > retries || err != PoolingException.Pooling) {
-                    throw err
-                }
-                (errorCount + 1)
-            }).flatMap { retryCount ->
-//                val mathPow = Math.pow(4.toDouble(), retryCount.toDouble())
-                Observable.interval(1, TimeUnit.SECONDS)
-            }
+            errors
+                    .scan(-1, { errorCount, err ->
+                        if (errorCount > retries || err != PollingException.Polling) {
+                            throw err
+                        }
+                        (errorCount + 1)
+                    })
+                    .filter { it >= 0 }
+                    .flatMap { retryCount ->
+                        //                val mathPow = Math.pow(4.toDouble(), retryCount.toDouble())
+                        Observable.interval(1, TimeUnit.SECONDS,scheduler).take(1)
+                    }
         }
     }
 }
 
+///**
+// * Transformer fornecido no exemplo da documentação do retry when
+// * */
+//class Transformer2 : ObservableTransformer<Any, Any> {
+//    override fun apply(upstream: Observable<Any>): ObservableSource<Any> {
+//        return upstream.retryWhen { attempts ->
+//            attempts.zipWith(Observable.range(1, 3), BiFunction<Throwable, Int, Int> { _, i: Int -> i }).flatMap({ i: Int ->
+//                println("delay retry by $i second(s)")
+//                Observable.timer(i.toLong(), TimeUnit.SECONDS)
+//            })
+//        }
+//    }
+//}
