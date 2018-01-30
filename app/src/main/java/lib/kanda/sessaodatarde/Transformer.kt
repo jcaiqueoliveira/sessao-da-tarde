f5b0fbcacb68a3b3655e7388fa72f338486043e0
Caique Oliveira <j.caique_fla@hotmail.com>
2018-01-30 21:19:20 -0200

    removed unused transformer and update code indent

--- app/src/main/java/lib/kanda/sessaodatarde/Transformer.kt
+++ app/src/main/java/lib/kanda/sessaodatarde/Transformer.kt
@@ -11,13 +11,15 @@ import java.util.concurrent.TimeUnit
 
 /**
  * Created by jcosilva on 1/24/2018.
- * Este transformer deve receber um estado de Polling e executar um número de tentativas que podem ser definidas no tra
- * Para o desenvolvimento da solução foi pesquisado sobre backOff Rx
- * Foi criado uma exception para representar um estado de Polling, dado que um pooling acorreu é feito um numero N de retentativas,
- * as retentativas continuam mesmo que ocorra uma exception diferente de Polling
+ * Este transformer deve receber um estado de Polling e executar um número de tentativas
+ * que podem ser definidas no tra para o desenvolvimento da solução foi pesquisado sobre backOff
+ * Foi criado uma exception para representar um estado de Polling, dado que um pooling acorreu é
+ * feito um numero N de retentativas, as retentativas continuam mesmo que ocorra uma exception
+ * diferente de Polling
  **/
 
-class Transformer1(val judge: Judge, val scheduler: Scheduler = Schedulers.computation()) : ObservableTransformer<Any, Any> {
+class Transformer1(val judge: Judge, val scheduler: Scheduler = Schedulers.computation()) :
+        ObservableTransformer<Any, Any> {
     private val DEFAULT_VALUE = 0
     private val FIRST = 1L
 
@@ -30,31 +32,16 @@ class Transformer1(val judge: Judge, val scheduler: Scheduler = Schedulers.compu
                             RETRY_STATUS.DONT_NEED_RETRY -> throw err
                         }
                     })
-                    .filter { it > 0 } //remover para uma função filtro
+                    .filter { it > 0 }
                     .switchMap { retryCount ->
                         Observable
-                                .interval(judge.timeToRetry(retryCount), TimeUnit.MILLISECONDS, scheduler)
+                                .interval(
+                                        judge.timeToRetry(retryCount),
+                                        TimeUnit.MILLISECONDS,
+                                        scheduler
+                                )
                                 .take(FIRST)
                     }
-        }
-    }
-}
-
-
-class Transformer2(val judge: Judge) : ObservableTransformer<Any, Any> {
-    private val INITIAL_VALUE = 0
-    override fun apply(upstream: Observable<Any>): ObservableSource<Any> {
-        return upstream.retryWhen { attempts ->
-            attempts.zipWith(Observable.range(INITIAL_VALUE, Int.MAX_VALUE),
-                    BiFunction<Throwable, Int, Int> { err, count ->
-                        return@BiFunction when (judge.checkIfRetryIsNeeded(count, err)) {
-                            RETRY_STATUS.NEED_RETRY -> (count + 1)
-                            RETRY_STATUS.DONT_NEED_RETRY -> throw err
-                        }
-                    }
-            ).flatMap({ count ->
-                Observable.timer(judge.timeToRetry(count), TimeUnit.SECONDS)
-            })
         }
     }
 }
